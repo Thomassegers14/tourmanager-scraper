d73b1cbef5532494c8a81b1038ebc3d76ea3adf0
Thomassegers14 <segers.thomas@telenet.be>
2025-08-24 16:56:45 +0200

    filter tests from selections

--- scripts/process_selections.R
+++ scripts/process_selections.R
@@ -1,7 +1,7 @@
 source("config/config.R")
 source("R/get_selections.R")
 
-selections <- get_selections() %>% process_selections()
+selections <- get_selections() %>% process_selections() %>% filter(!grepl("test", achternaam))
 eventId <- "vuelta-a-espana"
 eventYear <- 2025
 
